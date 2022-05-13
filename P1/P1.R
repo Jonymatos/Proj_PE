346ff418d13fdf0ad646d90dedda8a11f1bab2df
João Rui Matos <joaoruimatos@gmail.com>
2022-05-13 15:03:09 +0100

    I beg you, make it stop

--- P1/P1.R
+++ P1/P1.R
@@ -6,7 +6,8 @@ library(tidyr)
 df <- read.xlsx("ResiduosPerCapita.xlsx", sheet = 1, startRow = 12, cols = c(1,2,3))
 colnames(df) <- c("Países", "2004", "2018")
 df <- df[df$Países == "SE - Suécia" | df$Países == "GR - Grécia" | df$Países == "RO - Roménia", ]
-df <- transform(df, as.numeric(2004), as.numeric(2018))
 df_ano <- df %>% pivot_longer(c("2004", "2018"), names_to = "Ano", values_to = "Resíduos")
+df_ano <- transform(df_ano, Resíduos = as.numeric(Resíduos))
 ggplot(df_ano, aes(fill=Ano, y=Resíduos, x=Países)) + geom_bar(position="dodge", stat="identity") + 
   ggtitle("Resíduos per Capita")
+
