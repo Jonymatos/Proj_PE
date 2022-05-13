1e710c3a7051409c6590dda7e64ebd5ff2b80c54
João Rui Matos <joaoruimatos@gmail.com>
2022-05-13 14:42:50 +0100

    Slight improvements added on P1

--- P1/P1.R
+++ P1/P1.R
@@ -4,8 +4,9 @@ library(tidyr)
 
 
 df <- read.xlsx("ResiduosPerCapita.xlsx", sheet = 1, startRow = 12, cols = c(1,2,3))
-colnames(df) <- c("Paises", "ano_2004", "ano_2018")
-df <- df[df$Paises == "SE - Suécia" | df$Paises == "GR - Grécia" | df$Paises == "RO - Roménia", ]
+colnames(df) <- c("Países", "ano_2004", "ano_2018")
+df <- df[df$Países == "SE - Suécia" | df$Países == "GR - Grécia" | df$Países == "RO - Roménia", ]
 df <- transform(df, "ano_2004" = as.numeric(ano_2004), "ano_2018" = as.numeric(ano_2018))
-df_ano <- df %>% pivot_longer(c("ano_2004", "ano_2018"), names_to = "ano", values_to = "residuos")
-ggplot(df_ano, aes(fill=ano, y=residuos, x=Paises)) + geom_bar(position="dodge", stat="identity") + ggtitle("Residuos per Capita")
+df_ano <- df %>% pivot_longer(c("ano_2004", "ano_2018"), names_to = "ano", values_to = "Resíduos")
+ggplot(df_ano, aes(fill=ano, y=Resíduos, x=Países)) + geom_bar(position="dodge", stat="identity") + 
+  ggtitle("Resíduos per Capita")
