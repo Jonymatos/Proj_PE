b6b75756e3cf24f7a450ae4e7c58496ee987492a
João Rui Matos <joaoruimatos@gmail.com>
2022-05-13 14:57:40 +0100

    Ok, i promise this is the last modification...

--- P1/P1.R
+++ P1/P1.R
@@ -4,9 +4,9 @@ library(tidyr)
 
 
 df <- read.xlsx("ResiduosPerCapita.xlsx", sheet = 1, startRow = 12, cols = c(1,2,3))
-colnames(df) <- c("Países", "ano_2004", "ano_2018")
+colnames(df) <- c("Países", "2004", "2018")
 df <- df[df$Países == "SE - Suécia" | df$Países == "GR - Grécia" | df$Países == "RO - Roménia", ]
-df <- transform(df, "ano_2004" = as.numeric(ano_2004), "ano_2018" = as.numeric(ano_2018))
-df_ano <- df %>% pivot_longer(c("ano_2004", "ano_2018"), names_to = "ano", values_to = "Resíduos")
-ggplot(df_ano, aes(fill=ano, y=Resíduos, x=Países)) + geom_bar(position="dodge", stat="identity") + 
+df <- transform(df, as.numeric(2004), as.numeric(2018))
+df_ano <- df %>% pivot_longer(c("2004", "2018"), names_to = "Ano", values_to = "Resíduos")
+ggplot(df_ano, aes(fill=Ano, y=Resíduos, x=Países)) + geom_bar(position="dodge", stat="identity") + 
   ggtitle("Resíduos per Capita")
