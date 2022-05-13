fb1dc24d774d63c4f4acee5315e40530fa7d6dba
João Rui Matos <joaoruimatos@gmail.com>
2022-05-13 14:35:36 +0100

    P1 plot fix

--- P1/P1.R
+++ P1/P1.R
@@ -4,7 +4,8 @@ library(tidyr)
 
 
 df <- read.xlsx("ResiduosPerCapita.xlsx", sheet = 1, startRow = 12, cols = c(1,2,3))
-colnames(df) <- c("Paises", 2004, 2018)
+colnames(df) <- c("Paises", "ano_2004", "ano_2018")
 df <- df[df$Paises == "SE - Suécia" | df$Paises == "GR - Grécia" | df$Paises == "RO - Roménia", ]
-df_ano <- df %>% pivot_longer(c("2004", "2018"), names_to = "ano", values_to = "residuos")
-ggplot(df_ano, aes(fill=ano, y=residuos, x=Paises)) + geom_bar(position="dodge", stat="identity")
+df <- transform(df, "ano_2004" = as.numeric(ano_2004), "ano_2018" = as.numeric(ano_2018))
+df_ano <- df %>% pivot_longer(c("ano_2004", "ano_2018"), names_to = "ano", values_to = "residuos")
+ggplot(df_ano, aes(fill=ano, y=residuos, x=Paises)) + geom_bar(position="dodge", stat="identity") + ggtitle("Residuos per Capita")
