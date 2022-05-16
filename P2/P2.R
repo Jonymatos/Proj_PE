3cf6619e33631503bbd07b4b96bc87e2ccd54a11
João Rui Matos <joaoruimatos@gmail.com>
2022-05-16 13:07:08 +0100

    Final version of P2 done

--- P2/P2.R
+++ P2/P2.R
@@ -6,9 +6,9 @@ library(tidyr)
 df <- read.xlsx("EsperancaVida.xlsx", sheet = 1, startRow = 9, sep.names=" ")
 df <- df[df$X1 %in% c(2002:2019), ]
 df <- df[, (colnames(df) %in% c("X1", "EE - Estónia", "LU - Luxemburgo", "SE - Suécia"))]
-df <- select(df, -c(2:4))
-names(df) <- c("Anos", "Estónia M", "Luxemburgo M", "Suécia M", "Estónia F", "Luxemburgo F", "Suécia F") 
-df[] <- lapply(df, as.numeric)
-df_limpo <- df %>% pivot_longer(c("Estónia M", "Luxemburgo M", "Suécia M", "Estónia F", "Luxemburgo F", "Suécia F"), names_to = "Grupo", values_to = "Esperança")
-ggplot(df_limpo, aes(group = Grupo, x = Anos, y = Esperança)) + geom_line(aes(color = Grupo)) +
-  ggtitle("Esperança de Vida")
+df <- df %>% select(-c(2:4))
+names(df) <- c("Anos", "Estónia H", "Luxemburgo H", "Suécia H", "Estónia M", "Luxemburgo M", "Suécia M") 
+df[] <- df %>% lapply(as.numeric)
+df_limpo <- df %>% pivot_longer(names(.)[2:7], names_to = "Grupo", values_to = "Esperança")
+ggplot(df_limpo, aes(group = Grupo, x = Anos, y = Esperança)) + geom_line(aes(color = Grupo)) + 
+  geom_point(aes(color = Grupo)) + ggtitle("Esperança de Vida")
