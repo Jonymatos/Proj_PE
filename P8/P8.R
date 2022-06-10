af52a4531940383bf461b1de4331c0a71428f6b7
João Rui Matos <joaoruimatos@gmail.com>
2022-06-10 15:12:59 +0100

    P8 done as well

---
+++ P8/P8.R
@@ -0,0 +1,14 @@
+set.seed(655)
+n = 1289
+p = 1.28
+amostras = 650
+dados <- vector("double", amostras)
+
+for (m in c(1:amostras)){
+  lambda = 1/mean(rexp(n, p))
+  baixo = lambda * (1 - (qnorm(1-(0.07/2), mean=0, sd=1)) / sqrt(n))
+  alto = lambda * (1 + (qnorm(1-(0.07/2), mean=0, sd=1)) / sqrt(n))
+  dados[m] <- (alto - baixo)
+}
+
+mean(dados)
